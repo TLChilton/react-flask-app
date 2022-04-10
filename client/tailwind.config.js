3203dd844cfd71470021ec6ef065eabfeb7d5124
spewmaker <sranheman2013@gmail.com>
2022-04-09 20:41:22 -0700

    added a react front-end to consume the flask services

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      padding: ['hover'],
+      colors:{
+        blue:{
+          450: '#5F99F7'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
