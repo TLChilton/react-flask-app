dd8116c0cceca83943f7bcac0050bc9d179f0f00
TLChilton <thomaschilton93@gmail.com>
2022-04-13 15:59:20 -0700

    Added chart output and download as csv button

--- Thomas-Experiment/app.py
+++ Thomas-Experiment/app.py
@@ -3,8 +3,9 @@ import pandas as pd
 from tensorflow import keras
 from math import floor
 from sklearn.preprocessing import MinMaxScaler
-from flask import Flask, flash, request, jsonify, render_template, redirect, url_for
+from flask import Flask, flash, request, Response, render_template
 from werkzeug.utils import secure_filename
+from matplotlib import pyplot
 import os
 
 ALLOWED_EXTENSIONS = {'csv'}
@@ -19,15 +20,13 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
 @app.route('/',methods=['POST', 'GET'])
 def predict():
     if 'file' not in request.files:
         flash('No file part')
-        return render_template('index.html', error_text="No file found")
+        return render_template('index.html', error_text="")
+
     file = request.files['file']
     print(file.filename)
     if file and allowed_file(file.filename):
@@ -49,8 +48,28 @@ def predict():
         yhat = model.predict(predict_X)
         yhat = yhat.reshape(-1,1)
 
-        #return render_template('index.html', prediction_text='Successful prediction')
-        return jsonify(yhat.tolist())
+        # Finding Quarterly Maxes in Prediction
+        numQuarters = floor(365 / 90)
+        quarter = floor(365/numQuarters)
+        print(quarter)
+        maxes = [np.argmax(yhat[0:quarter], axis=0)]
+        for i in range(0, numQuarters-1):
+            maxes.append(np.argmax(yhat[quarter*(i+1):quarter*(i+2)-1], axis=0)+quarter*(i+1))
+        pyplot.figure(figsize=(11,4))
+
+        pyplot.plot(yhat, color='b', label="Prediction", zorder=2)
+        # Adding dashed lines for the quarterly maximums
+        pyplot.axvline(x=maxes[0], color='r', linestyle='--', label='Quarterly Max', zorder=1)
+        for xc in maxes[1:]:
+            pyplot.axvline(x=xc, color='r', linestyle='--', zorder=1)
+        pyplot.legend(loc='upper left')
+        pyplot.title("Prediction with Quarterly Maxes")
+
+        pyplot.tight_layout()
+        pyplot.savefig('static/images/chart.png')
+        np.savetxt("uploads/Prediction.csv", yhat, delimiter=',')
+
+        return render_template('results.html', url = 'static/images/chart.png')
     elif file.filename == '':
         flash('No selected file')
         return render_template('index.html', error_text="No selected file")
@@ -59,13 +78,18 @@ def predict():
         return render_template('index.html', error_text="Not a csv file")
     return render_template('index.html', error_text="An unexpected error occured")
 
-@app.route('/results',methods=['POST'])
+@app.route('/getPlotCSV',methods=['POST', 'GET'])
 def results():
 
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    with open("uploads/Prediction.csv") as fp:
+         csv = fp.read()
 
-    return jsonify(prediction)
+    return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition":
+                 "attachment; filename=myplot.csv"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
