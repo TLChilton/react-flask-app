e4f4ad4f1b3bff3fc1678e66d62884c2bb2496b7
TLChilton <thomaschilton93@gmail.com>
2022-04-15 16:56:53 -0700

    Added stock ticker and date input

--- Thomas-Experiment/app.py
+++ Thomas-Experiment/app.py
@@ -11,7 +11,7 @@ import dataDownload as dd  # dataDownload.py file uses pytrends library
 from tensorflow import keras
 from math import floor
 from sklearn.preprocessing import MinMaxScaler
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, request
 from matplotlib import pyplot
 import datetime
 from pandas.tseries.holiday import USFederalHolidayCalendar
@@ -102,103 +102,114 @@ def home():
 
 @app.route("/predict", methods=["POST", "GET"])
 def predict():
-    global GTRENDS_CACHE
-    # Download stock data
-    df_GM = pdr.get_data_tiingo("GM", api_key=key)
-    df_GM.index = df_GM.index.droplevel(0)
-    df_GM.index = pd.to_datetime(df_GM.index)
-    # Get today's date and a date 280 trading days in the past
-    today = datetime.datetime.today()
-    past = (today - 281 * US_BUSINESS_DAY).to_pydatetime()
+    if request.method == 'POST':
+        global GTRENDS_CACHE
+        # Download stock data
+        stock = request.form['stock']
+        print(stock, flush=True)
+        try:
+            df_Stock = pdr.get_data_tiingo(stock, api_key=key)
+        except pdr._utils.RemoteDataError:
+            return render_template("index.html", error_text="Invalid Stock Ticker. Stock '%s' not found" %(stock))
+        df_Stock.index = df_Stock.index.droplevel(0)
+        df_Stock.index = pd.to_datetime(df_Stock.index)
+        # The stock data is timezoned, so we have to make it untimezoned
+        df_Stock.index = df_Stock.index.tz_convert(None)
+        # Get inputed date and a date 280 trading days in the past
+        today = datetime.datetime.strptime(request.form['date'], '%Y-%m-%d')
+        past = df_Stock.loc[(df_Stock.index.to_pydatetime() <= today)].index[-281]
 
-    ''' Section for Google Trends
-     We check if the Google Trends data we need is available in the cached CSV
-     Otherwise we download it'''
-    # If our date is before the cache's last date
-    if GTRENDS_CACHE.iloc[-1:].index.to_pydatetime()[0] >= today:
-        # If our date is before the cache's first date
-        if today < GTRENDS_CACHE.iloc[0].index.to_pydatetime()[0]:
-            download = downloadTrends(past, GTRENDS_CACHE.iloc[0].index.to_pydatetime()[0])
+        ''' Section for Google Trends
+        We check if the Google Trends data we need is available in the cached CSV
+        Otherwise we download it'''
+        # If our date is before the cache's last date
+        if GTRENDS_CACHE.index[-1:] >= today:
+            # If our date is before the cache's first date
+            if today < GTRENDS_CACHE.index[0] or past < GTRENDS_CACHE.index[0]:
+                download = downloadTrends(past, GTRENDS_CACHE.index[0])
+                if download is None:
+                    past = GTRENDS_CACHE.index[0]
+                    today = (past + 281 * US_BUSINESS_DAY).to_pydatetime()
+                    df_trends = GTRENDS_CACHE.loc[(GTRENDS_CACHE.index <= today)]
+                else:
+                    df_fromCache = GTRENDS_CACHE.loc[(GTRENDS_CACHE.index >= past)]
+                    GTRENDS_CACHE = pd.concat([download, GTRENDS_CACHE]).drop_duplicates()
+                    GTRENDS_CACHE.to_csv("data/CachedGoogleTrends.csv")
+                    df_trends = pd.concat([download, df_fromCache]).drop_duplicates()
+            else:
+                df_trends = GTRENDS_CACHE.loc[(GTRENDS_CACHE.index <= today) & (GTRENDS_CACHE.index >= past)]
+        # If our date is after the cache's last date
+        else:
+            download = downloadTrends(GTRENDS_CACHE.index[-1:], today)
             if download is None:
-                past = GTRENDS_CACHE.iloc[0].index.to_pydatetime()[0]
-                today = (past + 281 * US_BUSINESS_DAY).to_pydatetime()
-                df_trends = GTRENDS_CACHE.loc[(GTRENDS_CACHE.index <= today)]
+                today = GTRENDS_CACHE.index[-1:]
+                past = (today - 281 * US_BUSINESS_DAY).to_pydatetime()
+                df_trends = GTRENDS_CACHE.loc[(GTRENDS_CACHE.index >= past)]
             else:
                 df_fromCache = GTRENDS_CACHE.loc[(GTRENDS_CACHE.index >= past)]
-                GTRENDS_CACHE = pd.concat([download, GTRENDS_CACHE]).drop_duplicates()
+                GTRENDS_CACHE = pd.concat([GTRENDS_CACHE, download]).drop_duplicates()
                 GTRENDS_CACHE.to_csv("data/CachedGoogleTrends.csv")
-                df_trends = pd.concat([download, df_fromCache]).drop_duplicates()
-        else:
-            df_trends = GTRENDS_CACHE.loc[(GTRENDS_CACHE.index <= today) & (GTRENDS_CACHE.index >= past)]
-    # If our date is after the cache's last date
-    else:
-        download = downloadTrends(GTRENDS_CACHE.iloc[-1:].index.to_pydatetime()[0], today)
-        if download is None:
-            today = GTRENDS_CACHE.iloc[-1:].index.to_pydatetime()[0]
-            past = (today - 281 * US_BUSINESS_DAY).to_pydatetime()
-            df_trends = GTRENDS_CACHE.loc[(GTRENDS_CACHE.index >= past)]
-        else:
-            df_fromCache = GTRENDS_CACHE.loc[(GTRENDS_CACHE.index >= past)]
-            GTRENDS_CACHE = pd.concat([GTRENDS_CACHE, download]).drop_duplicates()
-            GTRENDS_CACHE.to_csv("data/CachedGoogleTrends.csv")
-            df_trends = pd.concat([df_fromCache, download]).drop_duplicates()
+                df_trends = pd.concat([df_fromCache, download]).drop_duplicates()
 
-    # The stock data is timezoned, so we have to make it untimezoned
-    df_GM.index = df_GM.index.tz_convert(None)
-    # Merge datasets
-    df = df_GM.merge(df_trends, how="outer", left_index=True, right_index=True)
-    # Add day info for the model to use
-    df["Month"] = pd.DatetimeIndex(df.index).month
-    df["Day of the Month"] = pd.DatetimeIndex(df.index).day
-    df["Day of the Week"] = pd.DatetimeIndex(df.index).dayofweek
-    df["Day of the Year"] = pd.DatetimeIndex(df.index).dayofyear
-    df["Year"] = pd.DatetimeIndex(df.index).year
-    # Only keep columns we're using
-    df.drop(
-        df.columns.difference(["Date","close","EV Trend","Coronavirus Trend","Month","Day of the Month","Day of the Week","Day of the Year","Year",]),
-        1,
-        inplace=True,
-    )
-    df.dropna(inplace=True)
-    df_predict = df.tail(280)
-    values = df_predict.values
-    values = values.astype("float32")
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    predict_X = scaler.fit_transform(values)
-    predict_X = predict_X.reshape(-1, 280, len(df.columns))
-
-    # Make Prediction
-    yhat = model.predict(predict_X)
-    yhat = yhat.reshape(-1, 1)
-
-    # Finding Quarterly Maxes in Prediction
-    numQuarters = floor(365 / 90)
-    quarter = floor(365 / numQuarters)
-    maxes = [np.argmax(yhat[0:quarter], axis=0)]
-    for i in range(0, numQuarters - 1):
-        maxes.append(
-            np.argmax(yhat[quarter * (i + 1) : quarter * (i + 2) - 1], axis=0)
-            + quarter * (i + 1)
+        # Merge datasets
+        df = df_Stock.merge(df_trends, how="outer", left_index=True, right_index=True)
+        # Add day info for the model to use
+        df["Month"] = pd.DatetimeIndex(df.index).month
+        df["Day of the Month"] = pd.DatetimeIndex(df.index).day
+        df["Day of the Week"] = pd.DatetimeIndex(df.index).dayofweek
+        df["Day of the Year"] = pd.DatetimeIndex(df.index).dayofyear
+        df["Year"] = pd.DatetimeIndex(df.index).year
+        # Only keep columns we're using
+        df.drop(
+            df.columns.difference(["Date","close","EV Trend","Coronavirus Trend","Month","Day of the Month","Day of the Week","Day of the Year","Year",]),
+            1,
+            inplace=True,
         )
+        df[['EV Trend', 'Coronavirus Trend']] = df[['EV Trend', 'Coronavirus Trend']].fillna(value=0)
+        df.dropna(inplace=True)
+        df_predict = df.tail(280)
+        values = df_predict.values
+        values = values.astype("float32")
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        predict_X = scaler.fit_transform(values)
+        print(df_trends.shape)
+        print(predict_X.shape, flush=True)
+        predict_X = predict_X.reshape(-1, 280, len(df.columns))
 
-    # Create plot
-    pyplot.figure(figsize=(11, 4))
-    pyplot.plot(yhat, color="b", label="Prediction", zorder=2)
-    # Adding dashed lines for the quarterly maximums
-    pyplot.axvline(
-        x=maxes[0], color="r", linestyle="--", label="Quarterly Max", zorder=1
-    )
-    for xc in maxes[1:]:
-        pyplot.axvline(x=xc, color="r", linestyle="--", zorder=1)
-    pyplot.legend(loc="upper left")
-    pyplot.title("Prediction with Quarterly Maxes")
-    pyplot.tight_layout()
-    # Save plot to file
-    pyplot.savefig("static/images/chart.png")
-    # Save prediction data to CSV
-    np.savetxt("uploads/Prediction.csv", yhat, delimiter=",")
-    # Go to results page
-    return render_template("results.html", url="static/images/chart.png")
+        # Make Prediction
+        yhat = model.predict(predict_X)
+        yhat = yhat.reshape(-1, 1)
+
+        # Finding Quarterly Maxes in Prediction
+        numQuarters = floor(365 / 90)
+        quarter = floor(365 / numQuarters)
+        maxes = [np.argmax(yhat[0:quarter], axis=0)]
+        for i in range(0, numQuarters - 1):
+            maxes.append(
+                np.argmax(yhat[quarter * (i + 1) : quarter * (i + 2) - 1], axis=0)
+                + quarter * (i + 1)
+            )
+
+        # Create plot
+        pyplot.figure(figsize=(11, 4))
+        pyplot.plot(yhat, color="b", label="Prediction", zorder=2)
+        # Adding dashed lines for the quarterly maximums
+        pyplot.axvline(
+            x=maxes[0], color="r", linestyle="--", label="Quarterly Max", zorder=1
+        )
+        for xc in maxes[1:]:
+            pyplot.axvline(x=xc, color="r", linestyle="--", zorder=1)
+        pyplot.legend(loc="upper left")
+        pyplot.title("Prediction with Quarterly Maxes")
+        pyplot.tight_layout()
+        # Save plot to file
+        pyplot.savefig("static/images/chart.png")
+        # Save prediction data to CSV
+        np.savetxt("uploads/Prediction.csv", yhat, delimiter=",")
+        # Go to results page
+        return render_template("results.html", url="static/images/chart.png", Stock=stock)
+    else:
+        return f"The URL /predict was accessed directly. It only accepts POSTs"
 
 
 @app.route("/getPlotCSV", methods=["POST", "GET"])
