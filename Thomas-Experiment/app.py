4a7626e57422c19fdc62f6bc00426f31c111c83c
TLChilton <thomaschilton93@gmail.com>
2022-04-14 23:06:04 -0700

    Added functionality for downloading stock and google trends data

--- Thomas-Experiment/app.py
+++ Thomas-Experiment/app.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import pandas_datareader as pdr # for downloading stock data
+import dataDownload as dd   # dataDownload.py file uses pytrends library
 from tensorflow import keras
 from math import floor
 from sklearn.preprocessing import MinMaxScaler
@@ -7,12 +9,18 @@ from flask import Flask, flash, request, Response, render_template
 from werkzeug.utils import secure_filename
 from matplotlib import pyplot
 import os
+import datetime
+from pandas.tseries.holiday import USFederalHolidayCalendar
+from pandas.tseries.offsets import CustomBusinessDay
 
+# Business day calendar that includes weekends and US holidays (to find trading days)
+US_BUSINESS_DAY = CustomBusinessDay(calendar=USFederalHolidayCalendar())
 ALLOWED_EXTENSIONS = {'csv'}
 UPLOAD_FOLDER = 'uploads'
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+key = '5e6bbd0e3991e4888a436338a938fd961cd835ca'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 model = keras.models.load_model('model.h5')
 
@@ -20,63 +28,92 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['POST','GET'])
+def home():
+    return render_template('index.html')
 
-@app.route('/',methods=['POST', 'GET'])
+@app.route('/predict',methods=['POST', 'GET'])
 def predict():
-    if 'file' not in request.files:
-        flash('No file part')
-        return render_template('index.html', error_text="")
+    # Get today's date and a date 281 trading days in the past
+    today = datetime.datetime.today()
+    past = (today - 281 * US_BUSINESS_DAY).to_pydatetime().date()
+    # Download stock data
+    df_GM = pdr.get_data_tiingo('GM',api_key=key)
+    df_GM.index = df_GM.index.droplevel(0)
+    df_GM.index = pd.to_datetime(df_GM.index)
+    # Download Google Trends data
+    df_evTrend = dd.get_daily_data(
+        word="Electric vehicle",
+        start_year=past.year,
+        start_mon=past.month,
+        start_day=past.day,
+        stop_year=today.year,
+        stop_mon=today.month,
+        stop_day=today.day
+    )
+    df_evTrend.index = pd.to_datetime(df_evTrend.index)
+    df_evTrend.rename(columns={'Electric vehicle': 'EV Trend'}, inplace=True)
+    df_covidTrend = dd.get_daily_data(
+        word="Coronavirus",
+        start_year=past.year,
+        start_mon=past.month,
+        start_day=past.day,
+        stop_year=today.year,
+        stop_mon=today.month,
+        stop_day=today.day
+    )
+    df_covidTrend.index = pd.to_datetime(df_covidTrend.index)
+    df_covidTrend.rename(columns={'Coronavirus': 'Coronavirus Trend'}, inplace=True)
 
-    file = request.files['file']
-    print(file.filename)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'], filename), parse_dates=['Date'],index_col=0)
-        df.dropna(inplace=True)
-        df.drop(df.columns.difference(['Date','Close','Tesla Trend','Musk Trend','GM Trend','EV Trend','Coronavirus Trend','Month','Day of the Month','Day of the Week','Day of the Year','Year']), 1, inplace=True)
-        df_predict = df.drop(df.tail(365).index).tail(280)
-        values = df_predict.values
-        values = values.astype('float32')
-        scaler = MinMaxScaler(feature_range=(0,1))
-        predict_X = scaler.fit_transform(values)
-        predict_X = predict_X.reshape(-1, 280, len(df.columns))
+    # The stock data is timezoned, so we have to make it untimezoned
+    df_GM.index = df_GM.index.tz_convert(None)
+    # Merge datasets
+    df = df_GM.merge(df_evTrend, how='outer', left_index=True, right_index=True)
+    df = df.merge(df_covidTrend, how='outer', left_index=True, right_index=True)
+    # Add day info for the model to use
+    df['Month'] = pd.DatetimeIndex(df.index).month
+    df['Day of the Month'] = pd.DatetimeIndex(df.index).day
+    df['Day of the Week'] = pd.DatetimeIndex(df.index).dayofweek
+    df['Day of the Year'] = pd.DatetimeIndex(df.index).dayofyear
+    df['Year'] = pd.DatetimeIndex(df.index).year
+    # Only keep columns we're using
+    df.drop(df.columns.difference(['Date','close','EV Trend','Coronavirus Trend','Month','Day of the Month','Day of the Week','Day of the Year','Year']), 1, inplace=True)
+    df.dropna(inplace=True)
+    df_predict = df.tail(280)
+    values = df_predict.values
+    values = values.astype('float32')
+    scaler = MinMaxScaler(feature_range=(0,1))
+    predict_X = scaler.fit_transform(values)
+    predict_X = predict_X.reshape(-1, 280, len(df.columns))
 
-        print(predict_X.shape)
+    # Make Prediction
+    yhat = model.predict(predict_X)
+    yhat = yhat.reshape(-1,1)
 
-        # Make Prediction
-        yhat = model.predict(predict_X)
-        yhat = yhat.reshape(-1,1)
+    # Finding Quarterly Maxes in Prediction
+    numQuarters = floor(365 / 90)
+    quarter = floor(365/numQuarters)
+    print(quarter)
+    maxes = [np.argmax(yhat[0:quarter], axis=0)]
+    for i in range(0, numQuarters-1):
+        maxes.append(np.argmax(yhat[quarter*(i+1):quarter*(i+2)-1], axis=0)+quarter*(i+1))
 
-        # Finding Quarterly Maxes in Prediction
-        numQuarters = floor(365 / 90)
-        quarter = floor(365/numQuarters)
-        print(quarter)
-        maxes = [np.argmax(yhat[0:quarter], axis=0)]
-        for i in range(0, numQuarters-1):
-            maxes.append(np.argmax(yhat[quarter*(i+1):quarter*(i+2)-1], axis=0)+quarter*(i+1))
-        pyplot.figure(figsize=(11,4))
-
-        pyplot.plot(yhat, color='b', label="Prediction", zorder=2)
-        # Adding dashed lines for the quarterly maximums
-        pyplot.axvline(x=maxes[0], color='r', linestyle='--', label='Quarterly Max', zorder=1)
-        for xc in maxes[1:]:
-            pyplot.axvline(x=xc, color='r', linestyle='--', zorder=1)
-        pyplot.legend(loc='upper left')
-        pyplot.title("Prediction with Quarterly Maxes")
-
-        pyplot.tight_layout()
-        pyplot.savefig('static/images/chart.png')
-        np.savetxt("uploads/Prediction.csv", yhat, delimiter=',')
-
-        return render_template('results.html', url = 'static/images/chart.png')
-    elif file.filename == '':
-        flash('No selected file')
-        return render_template('index.html', error_text="No selected file")
-    elif not allowed_file(file.filename):
-        flash('Not a csv file')
-        return render_template('index.html', error_text="Not a csv file")
-    return render_template('index.html', error_text="An unexpected error occured")
+    # Create plot
+    pyplot.figure(figsize=(11,4))
+    pyplot.plot(yhat, color='b', label="Prediction", zorder=2)
+    # Adding dashed lines for the quarterly maximums
+    pyplot.axvline(x=maxes[0], color='r', linestyle='--', label='Quarterly Max', zorder=1)
+    for xc in maxes[1:]:
+        pyplot.axvline(x=xc, color='r', linestyle='--', zorder=1)
+    pyplot.legend(loc='upper left')
+    pyplot.title("Prediction with Quarterly Maxes")
+    pyplot.tight_layout()
+    # Save plot to file
+    pyplot.savefig('static/images/chart.png')
+    # Save prediction data to CSV
+    np.savetxt("uploads/Prediction.csv", yhat, delimiter=',')
+    # Go to results page
+    return render_template('results.html', url = 'static/images/chart.png')
 
 @app.route('/getPlotCSV',methods=['POST', 'GET'])
 def results():
