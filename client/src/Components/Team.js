fee28b64b5cc10014304cebe9419e10ae2a45765
Thomas Chilton <thomaschilton93@gmail.com>
2022-04-26 19:43:56 -0700

    Added basic page title, resized and recolored LinkedIn button

--- client/src/Components/Team.js
+++ client/src/Components/Team.js
@@ -2,51 +2,53 @@ import React from 'react'
 import Header from "./Header";
 
 function Team() {
-  return ( <><Header/><div class="row">
-  <div class="column" style={{alignItems: 'center', justifyContent: 'center'}}>
-    <div class="card">
-      <img src={require("./assets/Thomas.jpeg")} alt="Thomas Pic" style={{borderRadius: "50%", width:'50%', background: 'red', display: 'block'}}/>
-      <div class="container">
-        <h2>Thomas Chilton</h2>
-        <p class="title">Software Developer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p><form action = "https://www.linkedin.com/in/thomaschilton-cs/">
-              <input type="submit" class="button" value="LinkedIn"/>
-            </form>
-              </p>
+  return ( <><Header/>
+  <h1 style={{fontSize:'40px', margin:'10px'}}>Meet The Team</h1>
+  <div class="row">
+    <div class="column" style={{alignItems: 'center', justifyContent: 'center'}}>
+      <div class="card">
+        <img src={require("./assets/Thomas.jpeg")} alt="Thomas Pic" style={{borderRadius: "50%", width:'50%', background: 'red', display: 'block'}}/>
+        <div class="container">
+          <h2>Thomas Chilton</h2>
+          <p class="title">Software Developer</p>
+          <p hidden>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p><form action = "https://www.linkedin.com/in/thomaschilton-cs/">
+                <input type="submit" class="button" value="LinkedIn"/>
+              </form>
+                </p>
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="column">
-    <div class="card">
-      <img src={require("./assets/Heman.jpeg")} alt="Heman Pic" style={{borderRadius: "50%", width:'50%', background: 'red', display: 'block'}}/>
-      <div class="container">
-        <h2>Heman Sran</h2>
-        <p class="title">Software Developer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p><form action = "https://www.linkedin.com/in/hemansran/">
-              <input type="submit" class="button" value="LinkedIn"/>
-            </form>
-              </p>
+    <div class="column">
+      <div class="card">
+        <img src={require("./assets/Heman.jpeg")} alt="Heman Pic" style={{borderRadius: "50%", width:'50%', background: 'red', display: 'block'}}/>
+        <div class="container">
+          <h2>Heman Sran</h2>
+          <p class="title">Software Developer</p>
+          <p hidden>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p><form action = "https://www.linkedin.com/in/hemansran/">
+                <input type="submit" class="button" value="LinkedIn"/>
+              </form>
+                </p>
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="column">
-    <div class="card">
-      <img src={require("./assets/Pranav.jpg")} alt="Pranav Pic" style={{borderRadius: "50%", width:'50%', background: 'red', display: 'block'}}/>
-      <div class="container">
-        <h2>Pranav Annapareddi</h2>
-        <p class="title">Software Developer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p><form action = "https://www.linkedin.com/">
-              <input type="submit" class="button" value="LinkedIn"/>
-            </form>
-              </p>
+    <div class="column">
+      <div class="card">
+        <img src={require("./assets/Pranav.jpg")} alt="Pranav Pic" style={{borderRadius: "50%", width:'50%', background: 'red', display: 'block'}}/>
+        <div class="container">
+          <h2>Pranav Annapareddi</h2>
+          <p class="title">Software Developer</p>
+          <p hidden>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p><form action = "https://www.linkedin.com/in/pranav-annapareddi-50083a149/">
+                <input type="submit" class="button" value="LinkedIn"/>
+              </form>
+                </p>
+        </div>
       </div>
     </div>
-  </div>
 </div>
 <style dangerouslySetInnerHTML={{ 
   __html: `/* Three columns side by side */
@@ -81,6 +83,7 @@ function Team() {
               clear: both;
               display: table;
             }
+
             
             .title {
               color: grey;
@@ -92,10 +95,10 @@ function Team() {
               display: inline-block;
               padding: 8px;
               color: white;
-              background-color: #000;
+              background-color: #0997CB;
               text-align: center;
               cursor: pointer;
-              width: 100%;
+              width: 50%;
             }
             
             .button:hover {
